475816653fdc1b2d4c8ee9cb50701791350b6dac
Mayitzin <mayitzin@gmail.com>
2024-12-30 17:18:24 +0100

    Add information about ffmpeg's function showspectrumpic.

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,15 @@
 """
 Test Video and Frame manipulation with VideoWave
 
+New method with ffmpeg:
+
+  ffmpeg -i clip.mp4 -lavfi showspectrumpic=s=675x30:legend=False spectrogram.png
+
+- Useful to get the values faster than with the previous method.
+- It can get rid of the need for scipy for the spectrogram.
+- The options of showspectrumpic can be set to get the desired output. See
+  https://ffmpeg.org/ffmpeg-filters.html#showspectrumpic
+
 @author: Mario Garcia
 """
 
